6f6b5ac5bb1c47131eea898e419105a49c7e99a5
James Marshall <james_marshall48@aol.com>
2021-02-08 22:32:32 -0700

    Created SQLData interface

---
+++ src/SQLdata.java
@@ -0,0 +1,41 @@
+class SQLData implements DataInterface{
+
+    @Override
+    public void initializeDatabase(String filename) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void createEntry() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void readEntry() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void updateEntry() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteEntry() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void saveEntry() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+}
