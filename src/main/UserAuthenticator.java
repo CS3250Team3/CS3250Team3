dcb868f377f1375ad27ea25a3fad831e93c66e7d
Wolfy319 <jonwolffiorini@gmail.com>
2021-02-24 22:29:01 -0800

    Got the hashing working and wrote tests

--- src/main/UserAuthenticator.java
+++ src/main/UserAuthenticator.java
@@ -10,56 +10,63 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
  
 public class UserAuthenticator {
-
 	public User createUser(String username, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
 		User newUser = new User();
 		
+		newUser.setUsername(getEncryptedUsername(username));
 		newUser.setSalt(generateSalt());
 		newUser.setPassword(getEncryptedPassword(password, newUser.getSalt()));
-		
 		return newUser;
 	}
  
 	public boolean authenticate(String attemptedPassword, byte[] encryptedPassword, byte[] salt)
 	   throws NoSuchAlgorithmException, InvalidKeySpecException {
-	  // Encrypt the clear-text password using the same salt that was used to
-	  // encrypt the original password
+	  // Hash the clear-text password using the same salt that was used for the original hash
 	  byte[] encryptedAttemptedPassword = getEncryptedPassword(attemptedPassword, salt);
 	 
-	  // Authentication succeeds if encrypted password that the user entered
-	  // is equal to the stored hash
+	  // Authentication succeeds if encrypted password that the user entered is equal to the stored hash
 	  return Arrays.equals(encryptedPassword, encryptedAttemptedPassword);
 	 }
  
+	public byte[] getEncryptedUsername(String username) throws InvalidKeySpecException, NoSuchAlgorithmException {
+		String algorithm = "PBKDF2WithHmacSHA1"; // PBKDF2 with SHA-1 as the hashing algorithm
+		byte[] pepper = {0,0,0,0,0,0,0,0}; // Use the same salt for every username for easy lookup
+		
+		// Tells key factory what type of key to make
+		KeySpec spec = new PBEKeySpec(username.toCharArray(), pepper, 2, 160);
+		
+		// Instantiate factory with the algorithm to be used
+		SecretKeyFactory f = SecretKeyFactory.getInstance(algorithm);
+		
+		// Return hashed username
+		return f.generateSecret(spec).getEncoded();
+	}
+	
 	public byte[] getEncryptedPassword(String password, byte[] salt)
-	   throws NoSuchAlgorithmException, InvalidKeySpecException {
-	  // PBKDF2 with SHA-1 as the hashing algorithm. Note that the NIST
-	  // specifically names SHA-1 as an acceptable hashing algorithm for PBKDF2
-	  String algorithm = "PBKDF2WithHmacSHA1";
-	  // SHA-1 generates 160 bit hashes, so that's what makes sense here
-	  int derivedKeyLength = 160;
-	  // Pick an iteration count that works for you. The NIST recommends at
-	  // least 1,000 iterations:
-	  // http://csrc.nist.gov/publications/nistpubs/800-132/nist-sp800-132.pdf
-	  // iOS 4.x reportedly uses 10,000:
-	  // http://blog.crackpassword.com/2010/09/smartphone-forensics-cracking-blackberry-backup-passwords/
-	  int iterations = 5000;
-	 
-	  KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, derivedKeyLength);
-	 
-	  SecretKeyFactory f = SecretKeyFactory.getInstance(algorithm);
-	 
-	  return f.generateSecret(spec).getEncoded();
-	 }
+	throws NoSuchAlgorithmException, InvalidKeySpecException {
+	  
+		String algorithm = "PBKDF2WithHmacSHA1"; // PBKDF2 with SHA-1 as the hashing algorithm
+		int derivedKeyLength = 160; // SHA-1 generates 160 bit hashes, so that's what makes sense here
+		int iterations = 5000; // How many times the hash get repeated
+		
+		// Tells key factory what type of key to make
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, derivedKeyLength);
+		
+		// Instantiate factory with the algorithm to be used
+		SecretKeyFactory f = SecretKeyFactory.getInstance(algorithm);
+		
+		// Return hashed password
+		return f.generateSecret(spec).getEncoded();
+	}
  
 	public byte[] generateSalt() throws NoSuchAlgorithmException {
-	  // VERY important to use SecureRandom instead of just Random
-	  SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-	 
-	  // Generate a 8 byte (64 bit) salt as recommended by RSA PKCS5
-	  byte[] salt = new byte[8];
-	  random.nextBytes(salt);
-	 
-	  return salt;
+		// Apparently important to use SecureRandom instead of just Random
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+ 
+		// Generate a 8 byte (64 bit) salt
+		byte[] salt = new byte[8];
+		random.nextBytes(salt);
+ 
+		return salt;
 	 }
 }
